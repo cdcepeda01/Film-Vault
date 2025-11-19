dcc9d9808b6f724b03dd3c427f3c77c96465865f
cdcepeda01 <cdcepeda@uniboyaca.edu.co>
2025-11-18 21:12:37 -0500

    Configurar base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Film-Vault/', // 👈 EXACTO el nombre del repo
+});
